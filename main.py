6b41bf5da284953ce3bbd6ad9396fc5dbd173890
JunioX7X <157654279+JunioX7X@users.noreply.github.com>
2025-02-25 19:40:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ users = []
 class User(BaseModel):
     name: str
     email: str
-
+    
+@app.get("/users")
+def get_users():
+    return {"users": users}
+    
 @app.post("/users")
 def create_user(user: User):
     users.append(user.dict())
